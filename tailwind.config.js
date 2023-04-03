d6fdac2a11375080a76c711fdd22b986e28d0536
omarnaboush122 <omarnaboush89@gmail.com>
2023-04-03 17:31:05 +0300

    add colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,14 @@
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
+    colors: {
+      burntSienna: "#D87D4A",
+      blackBean: "#101010",
+      brightGray: "#F1F1F1",
+      peach: "#FBAF85",
+      white: "#fff",
+      black: "#000",
+    },
     extend: {},
   },
   plugins: [],
