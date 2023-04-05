9f13b6a6c627d1bca9e6b374a8ce6befa7588b8b
omarnaboush122 <omarnaboush89@gmail.com>
2023-04-05 18:36:49 +0300

    style intro section to tablet and desktop screens

--- src/components/Home/Intro.jsx
+++ src/components/Home/Intro.jsx
@@ -3,22 +3,20 @@ import { Link } from "react-router-dom";
 const Intro = () => {
   return (
     <section className="home-intro">
-      <div className="container mx-auto flex flex-col justify-center items-center h-full text-white text-center px-6 border-t border-solid border-[rgb(255,255,255,0.2)]">
-        <div className="max-w-sm">
-          <h3 className="text-sm tracking-[10px] uppercase opacity-50">
-            new product
-          </h3>
-          <h1 className="text-4xl font-bold tracking-widest uppercase mt-4 mb-6">
-            XX99 Mark II HeadphoneS
-          </h1>
-          <p className="text-base font-medium opacity-75 mb-7">
-            Experience natural, lifelike audio and exceptional build quality
-            made for the passionate music enthusiast.
-          </p>
-          <Link className="bg-burntSienna text-white py-4 px-8 text-sm font-bold tracking-widest uppercase cursor-pointer">
-            see product
-          </Link>
-        </div>
+      <div className="container mx-auto flex flex-col justify-center items-center h-full text-white text-center px-6 border-t border-solid border-[rgb(255,255,255,0.2)] lg:text-left lg:items-start">
+        <h3 className="text-sm tracking-[10px] uppercase opacity-50">
+          new product
+        </h3>
+        <h1 className="text-4xl font-bold max-w-xs tracking-widest uppercase mt-4 mb-6 md:text-6xl md:tracking-[2px] md:max-w-md">
+          XX99 Mark II HeadphoneS
+        </h1>
+        <p className="text-base font-medium opacity-75 mb-7 max-w-xs md:max-w-sm">
+          Experience natural, lifelike audio and exceptional build quality made
+          for the passionate music enthusiast.
+        </p>
+        <Link className="bg-burntSienna text-white py-4 px-8 text-sm font-bold tracking-widest uppercase cursor-pointer hover:bg-peach transition-colors duration-300">
+          see product
+        </Link>
       </div>
     </section>
   );
