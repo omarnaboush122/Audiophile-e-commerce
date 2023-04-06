6c1f04bb1ca9b7428c2197658f294ef0062d2817
omarnaboush122 <omarnaboush89@gmail.com>
2023-04-06 19:00:59 +0300

    add speaker zx7 article to home page

---
+++ src/components/Home/SpeakerZx7.jsx
@@ -0,0 +1,14 @@
+import { Link } from "react-router-dom";
+
+const SpeakerZx7 = () => {
+  return (
+    <article className="speakerzx7 bg-brightGray text-black w-full py-28 px-6 rounded-lg md:px-16 lg:px-24">
+      <div className="flex flex-col justify-start items-start gap-8">
+        <h3 className=" text-3xl font-bold uppercase tracking-widest">zx7 speaker</h3>
+        <Link className="py-4 px-8 text-sm font-bold tracking-widest uppercase cursor-pointer border border-solid border-black hover:bg-black hover:text-white transition-colors duration-300">See Product</Link>
+      </div>
+    </article>
+  );
+}
+
+export default SpeakerZx7;
--- src/components/Home/SpeakersAndEarphones.jsx
+++ src/components/Home/SpeakersAndEarphones.jsx
@@ -1,3 +1,4 @@
+import SpeakerZx7 from "./SpeakerZx7";
 import SpeakerZx9 from "./SpeakerZx9";
 
 
@@ -7,6 +8,7 @@ const SpeakersAndEarphones = () => {
       <div className="container mx-auto px-6 md:px-0">
         <div className="flex flex-col justify-center items-center gap-6 text-center md:gap-9 lg:gap-12">
           <SpeakerZx9 />
+          <SpeakerZx7/>
         </div>
       </div>
     </section>
