539ddc17088617b9bb503c1fc98e9e3a5d5b5b7d
omarnaboush122 <omarnaboush89@gmail.com>
2023-04-06 22:41:55 +0300

    add earphones yx1 to home page

---
+++ src/components/Home/EarphonesYx1.jsx
@@ -0,0 +1,18 @@
+import Earphone from "../../assets/home/desktop/image-earphones-yx1.jpg";
+import { Link } from "react-router-dom";
+
+const EarphonesYx1 = () => {
+  return (
+    <article className="relative w-full grid grid-cols-1 gap-6 md:grid-cols-2 md:gap-3 lg:gap-8">
+      <div>
+        <img src={Earphone} alt="earphones-yx1" className="rounded-lg w-full" />
+      </div>
+      <div className="flex flex-col items-start justify-center gap-8 bg-brightGray text-black px-6 py-10 rounded-lg md:p-10 lg:px-24">
+        <h3 className="text-2xl font-bold uppercase tracking-widest md:text-3xl">yx1 earphones</h3>
+        <Link className="text-sm font-bold py-4 px-8 uppercase tracking-wider border border-solid border-black hover:bg-black hover:text-white transition-colors duration-300">see product</Link>
+      </div>
+    </article>
+  );
+}
+
+export default EarphonesYx1;
--- src/components/Home/SpeakersAndEarphones.jsx
+++ src/components/Home/SpeakersAndEarphones.jsx
@@ -1,3 +1,4 @@
+import EarphonesYx1 from "./EarphonesYx1";
 import SpeakerZx7 from "./SpeakerZx7";
 import SpeakerZx9 from "./SpeakerZx9";
 
@@ -8,7 +9,8 @@ const SpeakersAndEarphones = () => {
       <div className="container mx-auto px-6 md:px-0">
         <div className="flex flex-col justify-center items-center gap-6 text-center md:gap-9 lg:gap-12">
           <SpeakerZx9 />
-          <SpeakerZx7/>
+          <SpeakerZx7 />
+          <EarphonesYx1 />
         </div>
       </div>
     </section>
