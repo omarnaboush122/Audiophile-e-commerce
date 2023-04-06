b112219bebee4bb9f60ca85a8fd744fcc86a4b98
omarnaboush122 <omarnaboush89@gmail.com>
2023-04-06 18:36:48 +0300

    add speaker zx9 article to home page

--- src/components/Home/SpeakersAndEarphones.jsx
+++ src/components/Home/SpeakersAndEarphones.jsx
@@ -1,19 +1,12 @@
-import { Link } from "react-router-dom";
-import Speaker from "../../assets/home/desktop/image-speaker-zx9.png";
+import SpeakerZx9 from "./SpeakerZx9";
+
 
 const SpeakersAndEarphones = () => {
   return (
     <section className="my-32">
       <div className="container mx-auto px-6 md:px-0">
-        <div className="flex flex-col justify-center items-center gap-6 text-center">
-          <article className="bg-burntSienna text-white w-full py-14 px-6 flex flex-col gap-8 rounded-lg md:gap-16">
-            <img src={Speaker} alt="speaker-zx9" className="max-w-[10rem] mx-auto" />
-            <div className="flex flex-col justify-center items-center gap-6 max-w-sm mx-auto">
-              <h3 className=" text-4xl font-bold uppercase max-w-[260px] tracking-widest md:text-6xl">zx9 speaker</h3>
-              <p className="text-base font-medium opacity-75">Upgrade to premium speakers that are phenomenally built to deliver truly remarkable sound.</p>
-              <Link className="bg-black py-4 px-8 text-sm font-bold tracking-widest cursor-pointer">See Product</Link>
-            </div>
-          </article>
+        <div className="flex flex-col justify-center items-center gap-6 text-center md:gap-9 lg:gap-12">
+          <SpeakerZx9 />
         </div>
       </div>
     </section>
