e54cb2bee3c3be0662bd271bbd346bc582a1c4f3
omarnaboush122 <omarnaboush89@gmail.com>
2023-04-07 11:16:29 +0300

    style footer to tablet and desktop screens

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -5,10 +5,10 @@ import { FaTwitter, FaInstagram, FaFacebook } from "react-icons/fa";
 const Footer = () => {
   return (
     <footer className="bg-blackBean text-white">
-      <div className='w-[90vw] max-w-5xl mx-auto text-center pb-10'>
-        <div className="flex flex-col justify-center items-center">
-          <img src={Logo} alt="logo" className="py-12 border-t-4 border-solid border-burntSienna" />
-          <ul className="flex flex-col gap-4">
+      <div className='w-[90vw] max-w-5xl mx-auto text-center pb-12 md:text-left'>
+        <div className="flex flex-col justify-center items-center md:justify-start md:items-start lg:flex-row lg:justify-between lg:items-center">
+          <img src={Logo} alt="logo" className="py-12 border-t-4 border-solid border-burntSienna md:pt-14" />
+          <ul className="flex flex-col gap-4 md:flex-row md:items-center md:gap-8">
             <li className="text-sm font-bold tracking-widest uppercase hover:text-burntSienna transition-colors duration-300">
               <Link to="/">home</Link>
             </li>
@@ -23,18 +23,20 @@ const Footer = () => {
             </li>
           </ul>
         </div>
-        <div className="my-12">
-          <p className="font-medium opacity-50">
+        <div className="my-12 md:mt-8 md:mb-20">
+          <p className="font-medium opacity-50 max-w-xl mx-auto md:m-0">
             Audiophile is an all in one stop to fulfill your audio needs. We're a small team of music lovers and sound specialists who are devoted to helping you get the most out of personal audio. Come and visit our demo facility - we’re open 7 days a week.
           </p>
         </div>
-        <div className="mb-12">
-          <p className="font-bold opacity-50">Copyright 2021. All Rights Reserved</p>
-        </div>
-        <div className="flex justify-center items-center gap-4">
-          <FaFacebook size={30} className="cursor-pointer hover:text-burntSienna transition-colors duration-300" />
-          <FaTwitter size={30} className="cursor-pointer hover:text-burntSienna transition-colors duration-300" />
-          <FaInstagram size={30} className="cursor-pointer hover:text-burntSienna transition-colors duration-300" />
+        <div className="w-full flex flex-col gap-12 md:flex-row md:justify-between md:items-center">
+          <div>
+            <p className="font-bold opacity-50">Copyright 2021. All Rights Reserved</p>
+          </div>
+          <div className="flex justify-center items-center gap-4">
+            <FaFacebook size={30} className="cursor-pointer hover:text-burntSienna transition-colors duration-300" />
+            <FaTwitter size={30} className="cursor-pointer hover:text-burntSienna transition-colors duration-300" />
+            <FaInstagram size={30} className="cursor-pointer hover:text-burntSienna transition-colors duration-300" />
+          </div>
         </div>
       </div>
     </footer>
