d69ae6e1a80b650dabdf35478e4afe3b11980491
omarnaboush122 <omarnaboush89@gmail.com>
2023-04-10 22:18:21 +0300

    add products to local storage and get it on first load

--- src/Context.jsx
+++ src/Context.jsx
@@ -1,3 +1,4 @@
+import { useEffect } from "react";
 import { useState } from "react";
 import { createContext } from "react";
 
@@ -6,6 +7,14 @@ export const Context = createContext();
 
 const ContextProvider = ({ children }) => {
   const [products, setProducts] = useState([]);
+
+  useEffect(() => {
+    localStorage.setItem("products", JSON.stringify(products));
+  }, [products])
+
+  useEffect(() => {
+    setProducts(JSON.parse(localStorage.getItem("products")))
+  }, [])
 
   return <Context.Provider value={{ products, setProducts }}>{children}</Context.Provider>
 }
