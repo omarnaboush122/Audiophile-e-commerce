a6e5e6099726a867890e62f6b1877a9fdfa9271e
omarnaboush122 <omarnaboush89@gmail.com>
2023-04-10 14:59:46 +0300

    add product features

--- src/components/ProductDetail/ProductFeatures.jsx
+++ src/components/ProductDetail/ProductFeatures.jsx
@@ -1,10 +1,20 @@
 import React from 'react';
 
-const ProductFeatures = () => {
+const ProductFeatures = ({ features }) => {
+
+  const featuresArray = features.split("\n\n");
+
   return (
-    <article>
-      <h3></h3>
-    </article>
+    <section className='my-32'>
+      <div className=' w-[90vw] max-w-6xl mx-auto'>
+        <h1 className='text-3xl font-bold tracking-widest mb-8'>Features</h1>
+        {
+          featuresArray.map((feature,i) => (
+            <p key={i} className='font-medium opacity-50 mb-4'>{feature}</p>
+          ))
+        }
+      </div>
+    </section>
   );
 }
 
--- src/pages/ProductDetailPage.jsx
+++ src/pages/ProductDetailPage.jsx
@@ -2,17 +2,19 @@ import { useParams } from "react-router-dom";
 import data from "../data.json";
 import ProductDescription from "../components/ProductDetail/ProductDescription";
 import GoBack from "../components/ProductDetail/GoBack";
+import ProductFeatures from "../components/ProductDetail/ProductFeatures";
 
 const ProductDetailPage = () => {
 
   let { id } = useParams();
 
   const { mobile } = data[id - 1].categoryImage;
-  const { name, new: newProduct, description, price } = data[id - 1];
+  const { name, new: newProduct, description, price,features } = data[id - 1];
   return (
     <main>
       <GoBack />
       <ProductDescription img={mobile} name={name} newProduct={newProduct} description={description} price={price} />
+      <ProductFeatures features={features}/>
     </main>
   );
 }
