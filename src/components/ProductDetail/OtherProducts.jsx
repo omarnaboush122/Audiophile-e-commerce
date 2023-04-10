fb616711a083cada15f2d33c3af1359efc178788
omarnaboush122 <omarnaboush89@gmail.com>
2023-04-10 18:13:49 +0300

    add other products to product detail page

---
+++ src/components/ProductDetail/OtherProducts.jsx
@@ -0,0 +1,24 @@
+import { Link } from "react-router-dom";
+
+const OtherProducts = ({ others }) => {
+
+  const otherProductsElements = others.map((item, i) => (
+    <article key={i} className="flex flex-col gap-8">
+      <img src={item.image.desktop} alt={item.name} className="w-full h-full" />
+      <h4 className="text-2xl font-bold uppercase tracking-widest text-center">{item.name}</h4>
+      <Link to={item.link} className=" bg-burntSienna text-white text-sm font-bold w-fit mx-auto px-8 py-4 flex justify-center items-center uppercase tracking-wider transition-colors duration-300 hover:bg-peach">see product</Link>
+    </article>
+  ))
+  return (
+    <section className="my-32">
+      <div className="w-[90vw] max-w-6xl mx-auto">
+        <h3 className="text-2xl font-bold uppercase mb-10 text-center tracking-wider">you may also like</h3>
+        <div className="grid grid-cols-1 gap-14 md:grid-cols-3 md:gap-3">
+          {otherProductsElements}
+        </div>
+      </div>
+    </section>
+  );
+}
+
+export default OtherProducts;
--- src/pages/ProductDetailPage.jsx
+++ src/pages/ProductDetailPage.jsx
@@ -4,19 +4,21 @@ import ProductDescription from "../components/ProductDetail/ProductDescription";
 import GoBack from "../components/ProductDetail/GoBack";
 import ProductFeatures from "../components/ProductDetail/ProductFeatures";
 import ProductImages from "../components/ProductDetail/ProductImages";
+import OtherProducts from "../components/ProductDetail/OtherProducts";
 
 const ProductDetailPage = () => {
 
   let { id } = useParams();
 
   const { mobile } = data[id - 1].categoryImage;
-  const { name, new: newProduct, description, price, features, includes, gallery } = data[id - 1];
+  const { name, new: newProduct, description, price, features, includes, gallery, others } = data[id - 1];
   return (
     <main>
       <GoBack />
       <ProductDescription img={mobile} name={name} newProduct={newProduct} description={description} price={price} />
       <ProductFeatures features={features} includes={includes} />
       <ProductImages gallery={gallery} />
+      <OtherProducts others={others} />
     </main>
   );
 }
