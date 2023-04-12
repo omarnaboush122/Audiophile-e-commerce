835dd2c5a9a38061914206641e702ee1b61782f5
omarnaboush122 <omarnaboush89@gmail.com>
2023-04-12 14:29:04 +0300

    add caret color to inputs

--- src/components/Checkout/Form.jsx
+++ src/components/Checkout/Form.jsx
@@ -33,15 +33,15 @@ const Form = () => {
               <div className="flex flex-wrap gap-6">
                 <div className="flex flex-col gap-2 flex-1">
                   <label htmlFor="name" className="text-xs font-bold">Name</label>
-                  <input type="text" id="name" className="px-6 py-5 text-sm font-bold rounded-lg border border-solid border-[#cfcfcf] focus:border-burntSienna focus:outline-burntSienna" placeholder="Alexei Ward" />
+                  <input type="text" id="name" className="px-6 py-5 text-sm font-bold rounded-lg border border-solid border-[#cfcfcf] caret-burntSienna focus:border-burntSienna focus:outline-burntSienna" placeholder="Alexei Ward" />
                 </div>
                 <div className="flex flex-col gap-2 flex-1">
                   <label htmlFor="email" className="text-xs font-bold">Email Adress</label>
-                  <input type="email" id="email" className="px-6 py-5 text-sm font-bold rounded-lg border border-solid border-[#cfcfcf] focus:border-burntSienna focus:outline-burntSienna" placeholder="Alexei@mail.com" />
+                  <input type="email" id="email" className="px-6 py-5 text-sm font-bold rounded-lg border border-solid border-[#cfcfcf] caret-burntSienna focus:border-burntSienna focus:outline-burntSienna" placeholder="Alexei@mail.com" />
                 </div>
                 <div className="flex flex-col gap-2 w-full">
                   <label htmlFor="phoneNumber" className="text-xs font-bold">Phone Number</label>
-                  <input type="tel" id="phoneNumber" className="px-6 py-5 text-sm font-bold rounded-lg border border-solid border-[#cfcfcf] focus:border-burntSienna focus:outline-burntSienna" placeholder="+1 202-555-0136" />
+                  <input type="tel" id="phoneNumber" className="px-6 py-5 text-sm font-bold rounded-lg border border-solid border-[#cfcfcf] caret-burntSienna focus:border-burntSienna focus:outline-burntSienna" placeholder="+1 202-555-0136" />
                 </div>
               </div>
             </div>
@@ -50,19 +50,19 @@ const Form = () => {
               <div className="flex flex-wrap gap-6">
                 <div className="flex flex-col gap-2 w-full">
                   <label htmlFor="address" className="text-xs font-bold">Your Address</label>
-                  <input type="text" id="address" className="px-6 py-5 text-sm font-bold rounded-lg border border-solid border-[#cfcfcf] focus:border-burntSienna focus:outline-burntSienna" placeholder="1137 Williams Avenue " />
+                  <input type="text" id="address" className="px-6 py-5 text-sm font-bold rounded-lg border border-solid border-[#cfcfcf] caret-burntSienna focus:border-burntSienna focus:outline-burntSienna" placeholder="1137 Williams Avenue " />
                 </div>
                 <div className="flex flex-col gap-2 flex-1">
                   <label htmlFor="zip" className="text-xs font-bold">ZIP Code</label>
-                  <input type="text" id="zip" className="px-6 py-5 text-sm font-bold rounded-lg border border-solid border-[#cfcfcf] focus:border-burntSienna focus:outline-burntSienna" placeholder="10001" />
+                  <input type="text" id="zip" className="px-6 py-5 text-sm font-bold rounded-lg border border-solid border-[#cfcfcf] caret-burntSienna focus:border-burntSienna focus:outline-burntSienna" placeholder="10001" />
                 </div>
                 <div className="flex flex-col gap-2 flex-1">
                   <label htmlFor="city" className="text-xs font-bold">City</label>
-                  <input type="text" id="city" className="px-6 py-5 text-sm font-bold rounded-lg border border-solid border-[#cfcfcf] focus:border-burntSienna focus:outline-burntSienna" placeholder="New York" />
+                  <input type="text" id="city" className="px-6 py-5 text-sm font-bold rounded-lg border border-solid border-[#cfcfcf] caret-burntSienna focus:border-burntSienna focus:outline-burntSienna" placeholder="New York" />
                 </div>
                 <div className="flex flex-col gap-2 w-full">
                   <label htmlFor="country" className="text-xs font-bold">Country</label>
-                  <input type="text" id="country" className="px-6 py-5 text-sm font-bold rounded-lg border border-solid border-[#cfcfcf] focus:border-burntSienna focus:outline-burntSienna" placeholder="United States" />
+                  <input type="text" id="country" className="px-6 py-5 text-sm font-bold rounded-lg border border-solid border-[#cfcfcf] caret-burntSienna focus:border-burntSienna focus:outline-burntSienna" placeholder="United States" />
                 </div>
               </div>
             </div>
@@ -71,7 +71,7 @@ const Form = () => {
               <div className="flex flex-col gap-4 md:flex-row md:justify-between md:gap-0">
                 <h4 className="text-xs font-bold capitalize">payment method</h4>
                 <div className="flex flex-col gap-4">
-                  <div className="flex items-center gap-4 font-bold w-80 px-6 py-5 rounded-lg border border-solid border-[#cfcfcf] cursor-pointer" onClick={() => setPayment("e-money")}>
+                  <div className="flex items-center gap-4 font-bold px-6 py-5 rounded-lg border border-solid border-[#cfcfcf] cursor-pointer md:w-80" onClick={() => setPayment("e-money")}>
                     <div className="relative w-6 h-6 rounded-full border border-solid border-[#cfcfcf]">
                       {
                         payment === "e-money" ? <div className="absolute w-3 h-3 rounded-full top-1/2 left-1/2 bg-burntSienna -translate-x-1/2 -translate-y-1/2"></div> : null
@@ -79,7 +79,7 @@ const Form = () => {
                     </div>
                     e-Money
                   </div>
-                  <div className="flex items-center gap-4 font-bold w-80 px-6 py-5 rounded-lg border border-solid border-[#cfcfcf] cursor-pointer" onClick={() => setPayment("cash")}>
+                  <div className="flex items-center gap-4 font-bold px-6 py-5 rounded-lg border border-solid border-[#cfcfcf] cursor-pointer md:w-80" onClick={() => setPayment("cash")}>
                     <div className="relative w-6 h-6 rounded-full border border-solid border-[#cfcfcf]">
                       {
                         payment === "cash" ? <div className="absolute w-3 h-3 rounded-full top-1/2 left-1/2 bg-burntSienna -translate-x-1/2 -translate-y-1/2"></div> : null
@@ -94,11 +94,11 @@ const Form = () => {
                 payment === "e-money" ? <div className="flex flex-wrap gap-6 mt-8">
                   <div className="flex flex-col gap-2 flex-1">
                     <label htmlFor="e-money-number" className="text-xs font-bold">E-Money Number</label>
-                    <input type="text" id="e-money-number" className="px-6 py-5 text-sm font-bold rounded-lg border border-solid border-[#cfcfcf] focus:border-burntSienna focus:outline-burntSienna" placeholder="238521993" />
+                    <input type="text" id="e-money-number" className="px-6 py-5 text-sm font-bold rounded-lg border border-solid border-[#cfcfcf] caret-burntSienna focus:border-burntSienna focus:outline-burntSienna" placeholder="238521993" />
                   </div>
                   <div className="flex flex-col gap-2 flex-1">
                     <label htmlFor="e-money-pin" className="text-xs font-bold">E-Money PIN</label>
-                    <input type="text" id="e-money-pin" className="px-6 py-5 text-sm font-bold rounded-lg border border-solid border-[#cfcfcf] focus:border-burntSienna focus:outline-burntSienna" placeholder="6891" />
+                    <input type="text" id="e-money-pin" className="px-6 py-5 text-sm font-bold rounded-lg border border-solid border-[#cfcfcf] caret-burntSienna focus:border-burntSienna focus:outline-burntSienna" placeholder="6891" />
                   </div>
                 </div> : <div className="flex flex-col justify-center items-center text-center gap-6 mt-8 md:flex-row md:text-left">
                   <img src={Cash} alt="cash-on-delivery" className="w-12 h-12" />
