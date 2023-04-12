f38f6fc537b7ad465781be520149ffb7e223371d
omarnaboush122 <omarnaboush89@gmail.com>
2023-04-12 12:26:42 +0300

    complete payment method and toggle inputs base on payment state

--- src/components/Checkout/Form.jsx
+++ src/components/Checkout/Form.jsx
@@ -1,4 +1,5 @@
 import { useState } from "react";
+import Cash from "../../assets/checkout/icon-cash-on-delivery.svg";
 
 
 const Form = () => {
@@ -52,7 +53,7 @@ const Form = () => {
               <div className="flex flex-col gap-4 md:flex-row md:justify-between md:gap-0">
                 <h4 className="text-xs font-bold capitalize">payment method</h4>
                 <div className="flex flex-col gap-4">
-                  <div className="flex items-center gap-4 font-bold px-6 py-5 rounded-lg border border-solid border-[#cfcfcf] cursor-pointer" onClick={() => setPayment("e-money")}>
+                  <div className="flex items-center gap-4 font-bold w-80 px-6 py-5 rounded-lg border border-solid border-[#cfcfcf] cursor-pointer" onClick={() => setPayment("e-money")}>
                     <div className="relative w-6 h-6 rounded-full border border-solid border-[#cfcfcf]">
                       {
                         payment === "e-money" ? <div className="absolute w-3 h-3 rounded-full top-1/2 left-1/2 bg-burntSienna -translate-x-1/2 -translate-y-1/2"></div> : null
@@ -60,7 +61,7 @@ const Form = () => {
                     </div>
                     e-Money
                   </div>
-                  <div className="flex items-center gap-4 font-bold px-6 py-5 rounded-lg border border-solid border-[#cfcfcf] cursor-pointer" onClick={() => setPayment("cash")}>
+                  <div className="flex items-center gap-4 font-bold w-80 px-6 py-5 rounded-lg border border-solid border-[#cfcfcf] cursor-pointer" onClick={() => setPayment("cash")}>
                     <div className="relative w-6 h-6 rounded-full border border-solid border-[#cfcfcf]">
                       {
                         payment === "cash" ? <div className="absolute w-3 h-3 rounded-full top-1/2 left-1/2 bg-burntSienna -translate-x-1/2 -translate-y-1/2"></div> : null
@@ -69,7 +70,23 @@ const Form = () => {
                     Cash on Delivery
                   </div>
                 </div>
+
               </div>
+              {
+                payment === "e-money" ? <div className="flex flex-wrap gap-6 mt-8">
+                  <div className="flex flex-col gap-2 flex-1">
+                    <label htmlFor="e-money-number" className="text-xs font-bold">E-Money Number</label>
+                    <input type="text" id="e-money-number" className="px-6 py-5 text-sm font-bold rounded-lg border border-solid border-[#cfcfcf] focus:border-burntSienna focus:outline-burntSienna" placeholder="238521993" />
+                  </div>
+                  <div className="flex flex-col gap-2 flex-1">
+                    <label htmlFor="e-money-pin" className="text-xs font-bold">E-Money PIN</label>
+                    <input type="text" id="e-money-pin" className="px-6 py-5 text-sm font-bold rounded-lg border border-solid border-[#cfcfcf] focus:border-burntSienna focus:outline-burntSienna" placeholder="6891" />
+                  </div>
+                </div> : <div className="flex flex-col justify-center items-center text-center gap-6 mt-8 md:flex-row md:text-left">
+                  <img src={Cash} alt="cash-on-delivery" className="w-12 h-12" />
+                  <p className="font-medium opacity-50">The ‘Cash on Delivery’ option enables you to pay in cash when our delivery courier arrives at your residence. Just make sure your address is correct so that your order will not be cancelled.</p>
+                </div>
+              }
             </div>
           </article>
         </form>
